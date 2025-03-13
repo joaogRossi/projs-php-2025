fa435f76dee8ece52543f7544d07caa61e76dc14
Joao Guilherme Barbosa Rossi <ra2457031>
2025-03-13 10:58:33 -0300

    projeto php

---
+++ index1.php
@@ -0,0 +1,7 @@
+<?php 
+    $cidade = "Bauru"; //declarou a cidade
+    $dia = "01"; //declarou o dia
+    $mes = "Março"; //declarou o mês
+    $ano = "2025"; // declarou o ano
+    echo "<b>$cidade</b>, <i>$dia<i/> de $mes de $ano"; //imprime a string concatenando as variaveis previamente declaradas
+?>
